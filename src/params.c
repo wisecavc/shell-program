c18a74aa8d06b887f5e70c1fcd4d0814409954c1
wisecavc <137959931+wisecavc@users.noreply.github.com>
2025-01-21 13:27:33 -0800

    Added module description to params.c

--- src/params.c
+++ src/params.c
@@ -1,3 +1,11 @@
+/*
+ * The params module is very small and only contains a definition for a struct
+ * called “params” that represents parameters of the shell. The structure holds
+ * parameters representing both exit status and the last background process ID.
+ * The “Params” struct is referenced and its parameters are updated in a significant
+ * number of other modules as the shell continues to process commands [3].
+ */
+
 /* XXX DO NOT MODIFY THIS FILE XXX */
 #include <errno.h>
 #include <unistd.h>
